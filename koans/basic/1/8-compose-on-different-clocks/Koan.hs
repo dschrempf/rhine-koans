65f5fc23480596f2969c196e1c7128b440b17f2f
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 15:35:58 +0200

    1-8

--- koans/basic/1/8-compose-on-different-clocks/Koan.hs
+++ koans/basic/1/8-compose-on-different-clocks/Koan.hs
@@ -1,22 +1,21 @@
-{- | Compose on different clocks.
-
-Clocked signal functions on the same clock type can be composed,
-and they can be run with a value of that clock type.
-But often we want to run different components at different rates!
-
-To compose parts with different clocks,
-Rhine has the concept of a 'ResamplingBuffer'.
-This is a basic component that consumes and produces data on two different clocks.
-
-Two 'Rhine's can be composed with a resampling buffer if all clock types and data types match.
--}
+-- | Compose on different clocks.
+--
+-- Clocked signal functions on the same clock type can be composed, and they can be
+-- run with a value of that clock type. But often we want to run different
+-- components at different rates!
+--
+-- To compose parts with different clocks, Rhine has the concept of a
+-- 'ResamplingBuffer'. This is a basic component that consumes and produces data
+-- on two different clocks.
+--
+-- Two 'Rhine's can be composed with a resampling buffer if all clock types and
+-- data types match.
 module Koan where
 
 -- text
 import Data.Text (Text)
 import Data.Text as Text (unlines)
 import Data.Text.IO as Text (putStrLn)
-
 -- rhine
 import FRP.Rhine
 
@@ -45,33 +44,31 @@ produceMessage = arr $ const "Hello Rhine!"
 printMessage :: ClSF IO EverySecond [Text] ()
 printMessage = arrMCl $ Text.putStrLn . Text.unlines
 
-{- | A resampling buffer that consumes 'Text' at 5 times per second,
-     and outputs all collected 'Text's as a list every second.
-
-                                                 The clock on which input arrives
-                                                  |
-                                                  |         The clock on which output is produced
-                                                  |            |
-                                                  |            |       Input type
-                                                  |            |        |
-                                                  |            |        |     Output type
-                                                  |            |        |      |
-                                                  v            v        v      v
--}
+-- | A resampling buffer that consumes 'Text' at 5 times per second,
+--     and outputs all collected 'Text's as a list every second.
+--
+--                                                 The clock on which input arrives
+--                                                  |
+--                                                  |         The clock on which output is produced
+--                                                  |            |
+--                                                  |            |       Input type
+--                                                  |            |        |
+--                                                  |            |        |     Output type
+--                                                  |            |        |      |
+--                                                  v            v        v      v
 fiveToOne :: (Monad m) => ResamplingBuffer m FivePerSecond EverySecond Text [Text]
 fiveToOne = collect
 
 -- * Main program
 
-{- | The complete program will run on _two_ clocks simultaneously.
-     This is the composed clock.
-
-                                  One part is activated on this clock
-                                    |
-                                    |          The other part is activated on this clock
-                                    |              |
-                                    v              v
--}
+-- | The complete program will run on _two_ clocks simultaneously.
+--     This is the composed clock.
+--
+--                                  One part is activated on this clock
+--                                    |
+--                                    |          The other part is activated on this clock
+--                                    |              |
+--                                    v              v
 type MainClock = SequentialClock FivePerSecond EverySecond
 
 -- | Produce "Hello Rhine!" five times per second.
@@ -82,16 +79,15 @@ produceRhine = produceMessage @@ fivePerSecond
 printRhine :: Rhine IO EverySecond [Text] ()
 printRhine = printMessage @@ everySecond
 
-{- | Print five messages every second.
-
-The messages are produced every 200 ms, collected, and printed every second.
-
-The operators >-- and --> compose two rhines on the left and right,
-and a resampling buffer in the middle.
-Can you fill the gap?
--}
+-- | Print five messages every second.
+--
+-- The messages are produced every 200 ms, collected, and printed every second.
+--
+-- The operators >-- and --> compose two rhines on the left and right,
+-- and a resampling buffer in the middle.
+-- Can you fill the gap?
 mainRhine :: Rhine IO MainClock () ()
-mainRhine = produceRhine >-- _ --> printRhine
+mainRhine = produceRhine >-- fiveToOne --> printRhine
 
 main :: IO ()
 main = flow mainRhine
