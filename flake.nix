845c958f6e98a0222d1d6a923ea4e175b8c14467
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 08:35:12 +0200

    working flake (use upstream rhine and rhine-gloss)

--- flake.nix
+++ flake.nix
@@ -1,32 +1,64 @@
 {
-  description = "Rhine-koans presentation";
+  description = "Haskell development environment";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://rhine-koans.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "rhine-koans.cachix.org-1:cglDz0vWVge7HGENYsJRB6yU7+jZNXJyZ0Ud9Z0uW54="
-    ];
-  };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    nix-mkPandoc = {
-      url = "github:chisui/nix-mkPandoc";
-      flake = false;
-    };
-  };
+  inputs.rhine.url = "github:turion/rhine";
 
-  outputs = inputs@{ self, nixpkgs, nix-mkPandoc }:
-    {
-      packages.x86_64-linux = let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        default = import ./presentation { inherit pkgs nix-mkPandoc; };
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    , rhine
+    }:
+    let
+      theseHpkgNames = [
+        "rhine-koans"
+      ];
+      thisGhcVersion = "ghc96";
+      hOverlay = selfn: supern: {
+        haskell = supern.haskell // {
+          packageOverrides = selfh: superh:
+            supern.haskell.packageOverrides selfh superh //
+              {
+                rhine-koans = selfh.callCabal2nix "rhine-koans" ./. { };
+                # Upstream dependencies.
+                rhine = rhine.packages.x86_64-linux.rhine;
+                rhine-gloss = rhine.packages.x86_64-linux.rhine-gloss;
+              };
+        };
       };
-    };
+      overlays = [ hOverlay ];
+      perSystem = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            inherit overlays;
+          };
+          hpkgs = pkgs.haskell.packages.${thisGhcVersion};
+          hlib = pkgs.haskell.lib;
+          theseHpkgs = nixpkgs.lib.genAttrs theseHpkgNames (n: hpkgs.${n});
+          theseHpkgsDev = builtins.mapAttrs (_: x: hlib.doBenchmark x) theseHpkgs;
+        in
+        {
+          packages = theseHpkgs // { default = theseHpkgs.rhine-koans; };
+
+          devShells.default = hpkgs.shellFor {
+            packages = _: (builtins.attrValues theseHpkgsDev);
+            nativeBuildInputs = [
+              # Haskell toolchain.
+              hpkgs.cabal-fmt
+              hpkgs.cabal-install
+              hpkgs.haskell-language-server
+            ];
+            buildInputs = [ ];
+            doBenchmark = true;
+            withHoogle = true;
+          };
+        };
+    in
+    { overlays.default = nixpkgs.lib.composeManyExtensions overlays; }
+    // flake-utils.lib.eachDefaultSystem perSystem;
 }
