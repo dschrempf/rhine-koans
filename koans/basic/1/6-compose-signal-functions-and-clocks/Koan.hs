b8ad9d40348f7f8b8888cb5703815c6e039dde54
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 15:26:10 +0200

    1-6

--- koans/basic/1/6-compose-signal-functions-and-clocks/Koan.hs
+++ koans/basic/1/6-compose-signal-functions-and-clocks/Koan.hs
@@ -1,27 +1,26 @@
-{- | Compose signal functions with clocks.
-
-So far, we always had one clock, and added it to our program right at the end with the @@ operator.
-But what actually happens when we apply @@?
-
-* On its left side, it expects a clocked signal function.
-* The right side must be a clock value.
-* The clock types of both sides have to match.
-* The output of @@ is a type we haven't seen explicitly yet: it's called 'Rhine'!
-
-We end up with a complete program that consists of a signal function which produces, processes, and consumes data,
-and a clock which drives the signal function at a specific rate.
-
-But unfortunately, some very strange error messages have crept in.
-This is because we have used the general composition operator >>> instead of the Rhine-specific operator >->,
-and the type checker is confused which of @@ and >>> to apply first.
-Can you fix it?
--}
+-- | Compose signal functions with clocks.
+--
+-- So far, we always had one clock, and added it to our program right at the end
+-- with the @@ operator. But what actually happens when we apply @@?
+--
+-- * On its left side, it expects a clocked signal function.
+-- * The right side must be a clock value.
+-- * The clock types of both sides have to match.
+-- * The output of @@ is a type we haven't seen explicitly yet: it's called 'Rhine'!
+--
+-- We end up with a complete program that consists of a signal function which
+-- produces, processes, and consumes data, and a clock which drives the signal
+-- function at a specific rate.
+--
+-- But unfortunately, some very strange error messages have crept in. This is
+-- because we have used the general composition operator >>> instead of the
+-- Rhine-specific operator >->, and the type checker is confused which of @@ and
+-- >>> to apply first. Can you fix it?
 module Koan where
 
 -- text
 import Data.Text (Text)
 import Data.Text.IO as Text (putStrLn)
-
 -- rhine
 import FRP.Rhine
 
@@ -44,19 +43,18 @@ exclamate = arr (<> "!")
 printMessage :: ClSF IO EverySecond Text ()
 printMessage = arrMCl Text.putStrLn
 
-{- | A complete Rhine program that prints "Hello Rhine!" every second.
-
-                   The Rhine can perform side effects in IO.
-                   |
-                   |   It ticks every second.
-                   |   |
-                   |   |          It consumes and produces no data.
-                   |   |          |   |
-                   v   v          v   v
--}
+-- | A complete Rhine program that prints "Hello Rhine!" every second.
+--
+--                   The Rhine can perform side effects in IO.
+--                   |
+--                   |   It ticks every second.
+--                   |   |
+--                   |   |          It consumes and produces no data.
+--                   |   |          |   |
+--                   v   v          v   v
 mainRhine :: Rhine IO EverySecond () ()
 -- The operators >>> and @@ don't combine well. Try brackets, or the >-> operator!
-mainRhine = produceMessage >>> exclamate >>> printMessage @@ everySecond
+mainRhine = ((produceMessage >>> exclamate) >>> printMessage) @@ everySecond
 
 main :: IO ()
 main =
