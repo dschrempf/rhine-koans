bef7eed199be79f033149848bf7bf7e3dda512b8
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 15:54:48 +0200

    2-6

--- koans/basic/2/6-count-everything/Koan.hs
+++ koans/basic/2/6-count-everything/Koan.hs
@@ -1,19 +1,17 @@
-{- | Count everything!
-
-If we can count words, characters, and lines, let's just put it all together.
-This is a bit finicky, but give it a try nevertheless!
-Remember the combinator for parallel composition:
-
-@
-(&&&) :: Monad m => ClSF m cl a b -> ClSF m cl a c -> ClSF m cl a (b, c)
-@
--}
+-- | Count everything!
+--
+-- If we can count words, characters, and lines, let's just put it all together.
+-- This is a bit finicky, but give it a try nevertheless! Remember the combinator
+-- for parallel composition:
+--
+-- @
+-- (&&&) :: Monad m => ClSF m cl a b -> ClSF m cl a c -> ClSF m cl a (b, c)
+-- @
 module Koan where
 
 -- text
 import Data.Text (Text)
 import Data.Text qualified as Text (length, words)
-
 -- rhine
 import FRP.Rhine hiding (currentInput)
 
@@ -25,11 +23,10 @@ userInput = tagS
 wordCount :: ClSF IO StdinClock () Int
 wordCount = userInput >-> arr (Text.words >>> length)
 
-{- | Output the number of characters of the line that was just entered.
-
-The newline character is not part of 'userInput',
-therefore +1 is added for it.
--}
+-- | Output the number of characters of the line that was just entered.
+--
+-- The newline character is not part of 'userInput',
+-- therefore +1 is added for it.
 charCount :: ClSF IO StdinClock () Int
 -- Yes, you can use >>> to compose ordinary functions as well!
 charCount = userInput >-> arr (Text.length >>> (+ 1))
@@ -48,13 +45,16 @@ totalCharCount :: ClSF IO StdinClock () Int
 totalCharCount = charCount >-> sumN
 
 -- | The number of total lines, words and characters so far.
-totalCount :: ClSF IO StdinClock () _ -- What will the type of this be?
-totalCount = _ &&& _ &&& _
+totalCount :: ClSF IO StdinClock () (Integer, (Int, Int)) -- What will the type of this be?
+totalCount = lineCount &&& totalWordCount &&& totalCharCount
 
 -- | Print the number of total lines, words and characters so far.
 printAllCounts :: ClSF IO StdinClock () ()
 -- On what do you need to pattern match here to bring lines_, words_ and chars into scope?
-printAllCounts = totalCount >-> arrMCl (\_ -> print lines_ >> print words_ >> print chars)
+printAllCounts =
+  totalCount
+    >-> arrMCl
+      (\(lines_, (words_, chars)) -> print lines_ >> print words_ >> print chars)
 
 main :: IO ()
 main = flow $ printAllCounts @@ StdinClock
