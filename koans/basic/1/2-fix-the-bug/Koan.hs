f4a70538641588c4a9b752c8ea5025e5d4b671b4
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 15:06:23 +0200

    1-2

--- koans/basic/1/2-fix-the-bug/Koan.hs
+++ koans/basic/1/2-fix-the-bug/Koan.hs
@@ -1,20 +1,18 @@
-{- | Fix the bug!
-
-The second task is nearly the same as the first task:
-Your program should again output the message "Hello Rhine!" every second.
-But now, there is a bug in the code somewhere, preventing the program to compile!
-Can you spot it and fix it?
--}
+-- | Fix the bug!
+--
+-- The second task is nearly the same as the first task:
+-- Your program should again output the message "Hello Rhine!" every second.
+-- But now, there is a bug in the code somewhere, preventing the program to compile!
+-- Can you spot it and fix it?
 module Koan where
 
 -- text
 import Data.Text.IO as Text (putStrLn)
-
 -- rhine
 import FRP.Rhine
 
 -- | A clock that ticks every second.
-everySecond :: Millisecond 2345
+everySecond :: Millisecond 1000
 everySecond = waitClock
 
 -- | A component of the whole signal network.
