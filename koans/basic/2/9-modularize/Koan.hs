8fd346c58273e86221e1bad9c67fbc9e66174f9d
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 16:12:04 +0200

    2-9

--- koans/basic/2/9-modularize/Koan.hs
+++ koans/basic/2/9-modularize/Koan.hs
@@ -1,56 +1,42 @@
 {-# LANGUAGE Arrows #-}
 
-{- | Modularize.
-
-Let's clean up the code. Ideally, we don't have one big monolithic function,
-but rather several reusable, independent ones.
-A typical pattern is to separate computations from side effects, so let us do that here!
--}
+-- | Modularize.
+--
+-- Let's clean up the code. Ideally, we don't have one big monolithic function, but
+-- rather several reusable, independent ones. A typical pattern is to separate
+-- computations from side effects, so let us do that here!
 module Koan where
 
 -- text
 import Data.Text qualified as Text (length, words)
-
 -- rhine
 import FRP.Rhine hiding (currentInput)
 
 -- | Count the number of lines, words and chars.
 allCounts :: ClSF IO StdinClock () (Int, Int, Int)
-allCounts = _
-
--- | Print the three counts.
-printCounts :: ClSF IO StdinClock (Int, Int, Int) ()
-printCounts = proc (lineCount, totalWordCount, totalCharCount) -> do
-  _ -< _
-
--- | On every 1000th line, print the number of total lines, words and characters so far.
-printAllCounts :: ClSF IO StdinClock () ()
-printAllCounts = proc () -> do
-  counts@(lineCount, _, _) <- _ -< ()
-  if lineCount `mod` 1000 == 0
-    then _ -< counts
-    else returnA -< ()
-
--- For reference, here is the previous implementation.
--- Can you reuse its pieces to implement the holes above?
-
--- | On every 1000th line, print the number of total lines, words and characters so far.
-printAllCountsMonolith :: ClSF IO StdinClock () ()
-printAllCountsMonolith = proc () -> do
+allCounts = proc () -> do
   userInput <- tagS -< ()
-
   let wordCount = length $ Text.words userInput
       charCount = Text.length userInput + 1
 
   lineCount <- count @Int -< ()
   totalWordCount <- sumN -< wordCount
   totalCharCount <- sumN -< charCount
+  returnA -< (lineCount, totalWordCount, totalCharCount)
 
+-- | Print the three counts.
+printCounts :: ClSF IO StdinClock (Int, Int, Int) ()
+printCounts = proc (lineCount, totalWordCount, totalCharCount) -> do
+  arrMCl print -< lineCount
+  arrMCl print -< totalWordCount
+  arrMCl print -< totalCharCount
+
+-- | On every 1000th line, print the number of total lines, words and characters so far.
+printAllCounts :: ClSF IO StdinClock () ()
+printAllCounts = proc () -> do
+  counts@(lineCount, _, _) <- allCounts -< ()
   if lineCount `mod` 1000 == 0
-    then do
-      arrMCl print -< lineCount
-      arrMCl print -< totalWordCount
-      arrMCl print -< totalCharCount
+    then printCounts -< counts
     else returnA -< ()
 
 main :: IO ()
