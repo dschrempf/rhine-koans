0c63659bb0525f6f23d206890a394575f4a4a8ab
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 15:28:17 +0200

    1-7

--- koans/basic/1/7-compose-on-the-same-clock/Koan.hs
+++ koans/basic/1/7-compose-on-the-same-clock/Koan.hs
@@ -1,17 +1,15 @@
-{- | Compose on the same clock.
-
-You have seen before how important it is to get the rate of a clock right.
-What if we tried to compose components that are specified to run at different rates?
-This cannot work correctly! And therefore, it is forbidden in Rhine.
-See the type error for yourself!
-Can you fix the program such that all components run at one message per second again?
--}
+-- | Compose on the same clock.
+--
+-- You have seen before how important it is to get the rate of a clock right. What
+-- if we tried to compose components that are specified to run at different rates?
+-- This cannot work correctly! And therefore, it is forbidden in Rhine. See the
+-- type error for yourself! Can you fix the program such that all components run at
+-- one message per second again?
 module Koan where
 
 -- text
 import Data.Text (Text)
 import Data.Text.IO as Text (putStrLn)
-
 -- rhine
 import FRP.Rhine
 
@@ -23,15 +21,15 @@ everySecond :: EverySecond
 everySecond = waitClock
 
 -- | Produce an incomplete message.
-produceMessage :: ClSF IO (Millisecond 2345) () Text
+produceMessage :: ClSF IO EverySecond () Text
 produceMessage = arr $ const "Hello Rhine"
 
 -- | Add an exclamation mark ("!") to a Text.
-exclamate :: (Monad m) => ClSF m (Millisecond 3456) Text Text
+exclamate :: (Monad m) => ClSF m EverySecond Text Text
 exclamate = arr (<> "!")
 
 -- | Outputs a message every second.
-printMessage :: ClSF IO (Millisecond 4567) Text ()
+printMessage :: ClSF IO EverySecond Text ()
 printMessage = arrMCl Text.putStrLn
 
 -- | A complete Rhine program that prints "Hello Rhine!" every second.
