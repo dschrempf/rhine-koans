5fb03c8014825b546780cc67cdafcd4d6da0d8c6
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-06-08 15:06:26 +0200

    1-3

--- koans/basic/1/3-faster/Koan.hs
+++ koans/basic/1/3-faster/Koan.hs
@@ -1,24 +1,22 @@
-{- | Faster!
-
-Maybe you've already noticed: In Rhine, clock rates are specified _on the type level_.
-This means, that we can write clock types in a type signature, or also in a type synonym.
-Often, the code becomes more readable if we use type synonyms.
-
-Now, can you output the message "Hello Rhine!" _ten times_ per second?
--}
+-- | Faster!
+--
+-- Maybe you've already noticed: In Rhine, clock rates are specified _on the type level_.
+-- This means, that we can write clock types in a type signature, or also in a type synonym.
+-- Often, the code becomes more readable if we use type synonyms.
+--
+-- Now, can you output the message "Hello Rhine!" _ten times_ per second?
 module Koan where
 
 -- text
 import Data.Text.IO as Text (putStrLn)
-
 -- rhine
 import FRP.Rhine
 
 -- Can you complete this?
 
-{- | The clock _type_ specifies the rate of the clock.
-type TenPerSecond = Millisecond _
--}
+-- | The clock _type_ specifies the rate of the clock.
+-- type TenPerSecond = Millisecond _
+type TenPerSecond = Millisecond 100
 
 -- | A clock that ticks 10 times second.
 tenPerSecond :: TenPerSecond
